f12804639b4bd3fb48880c0c27406e2aa1a87c4d
Daniel A <soc13dan@student.lu.se>
2016-02-18 16:26:29 +0100

    added methods

---
+++ src/staff/Division.java
@@ -0,0 +1,13 @@
+package staff;
+
+import java.util.ArrayList;
+
+public class Division {
+	private ArrayList<StaffMember> members;
+	private String divisionId;
+	
+	public Division() {
+		
+	}
+	
+}
