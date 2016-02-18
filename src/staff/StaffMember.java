503be38c1a85bef3ab542ed5b057efd542891807
Daniel A <soc13dan@student.lu.se>
2016-02-18 15:35:54 +0100

    created classes for staff
    created classes for staff

---
+++ src/staff/StaffMember.java
@@ -0,0 +1,16 @@
+package staff;
+
+public class StaffMember {
+	private int id;
+	private String title;
+
+	public StaffMember(String title) {
+		this.title = title;
+		
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+}
